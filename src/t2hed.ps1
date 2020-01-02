3173825af0be345ccfbf6c7f6f461b929e7a1a83
Ellart Ott <48015293+KHKEllartOtt@users.noreply.github.com>
2020-01-02 09:32:42 -0800

    tähtede testimiseks loodud mini-skript

---
+++ src/t2hed.ps1
@@ -0,0 +1,9 @@
+﻿# T2pit2htede asendamiseks m6eldud test
+
+$tahed = 'Õunad on želatiinist ja šampoonist. Ärge sööge ÜHTEGI mürgist mõtetut ainet. Söö Šokolaadi!'
+$tahed
+Write '----------------------------------------------------------------------'
+
+$tahed = [Text.Encoding]::ASCII.GetString([Text.Encoding]::GetEncoding("Cyrillic").GetBytes($tahed))
+
+$tahed
